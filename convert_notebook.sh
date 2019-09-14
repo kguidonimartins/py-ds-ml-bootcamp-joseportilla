02e7be0bdd4bfdf453fc3735f1eb1979535ea223
Karlo Guidoni Martins <kguidonimartins@gmail.com>
2019-09-14 14:24:01 -0300

    update nb converter script

--- convert_notebook.sh
+++ convert_notebook.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+# How to use:
+# sh convert_notebook.sh path/to/file.ipynb
+
+# Catch file
+FILE=$1
 
 
 # convert all notebooks to markdown
-jupyter nbconvert --to markdown */*/*.ipynb
+echo
+echo Converting "$FILE" to MARKDOWN
+echo
+jupyter nbconvert --to markdown "$FILE"
 
 # convert all notebooks to python scripts
-jupyter nbconvert --to script */*/*.ipynb
+echo
+echo Converting "$FILE" to PYTHON
+echo
+jupyter nbconvert --to script "$FILE"
